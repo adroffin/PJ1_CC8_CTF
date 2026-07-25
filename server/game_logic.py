24ca7baabc3ee42fed3d94114a542f3887168888
adroffin <23004161@galileo.edu>
2026-07-25 01:26:22 -0600

    feat(engine): Implementacion maquina de estados para el flujo de la partida

--- server/game_logic.py
+++ server/game_logic.py
@@ -22,6 +22,10 @@ class GameEngine:
 
         self.is_game_over = False
         self.winner_name = None
+
+        self.game_state = "LOBBY"
+
+        self.countdown_start = None
         
         self.lock = threading.Lock()  # Candado para proteger la memoria compartida
         
@@ -77,11 +81,17 @@ class GameEngine:
                 del self.players[player_id]
                 print(f"[ENGINE] Jugador {player_id} eliminado del mundo.")
 
+    def get_player_count(self):
+        with self.lock:
+            return len(self.players)
+
     def update_player_input(self, player_id: str, dir_x: int, dir_y: int):
         """
         Guarda la última dirección enviada por el cliente.
         El movimiento real ocurre dentro del Game Loop.
         """
+        if self.game_state != "PLAYING":
+            return
         with self.lock:
             if player_id not in self.players:
                 return
@@ -91,6 +101,8 @@ class GameEngine:
 
     def handle_player_interact(self, player_id: str):
         """Procesa el intento de un jugador de agarrar o robar la bandera."""
+        if self.game_state != "PLAYING":
+            return
         with self.lock:
             if player_id not in self.players:
                 return
@@ -153,79 +165,128 @@ class GameEngine:
             # 1. Copiamos el estado actual usando el Lock para evitar colisiones de hilos
             with self.lock:
 
+                # Cambio de estado dentro del juego
+                if self.game_state == "LOBBY":
+                    if len(self.players) >= 2:
+                        self.game_state = "COUNTDOWN"
+                        self.countdown_start = time.perf_counter()
+                        print("[ENGINE] Iniciando cuenta regresiva...")
+
+                if self.game_state == "COUNTDOWN":
+                    elapsed = time.perf_counter() - self.countdown_start
+                    if elapsed >= 3:
+                        self.game_state = "PLAYING"
+                        print("[ENGINE] La partida ha comenzado.")
+
                 # Movimiento de jugadores
-                for player in self.players.values():
+                if self.game_state == "PLAYING":
+                    for player in self.players.values():
 
-                    dx = player["dir_x"]
-                    dy = player["dir_y"]
+                        dx = player["dir_x"]
+                        dy = player["dir_y"]
 
-                    if dx == 0 and dy == 0:
-                        continue
+                        if dx == 0 and dy == 0:
+                            continue
 
-                    magnitude = math.hypot(dx, dy)
+                        magnitude = math.hypot(dx, dy)
 
-                    if magnitude == 0:
-                        continue
+                        if magnitude == 0:
+                            continue
 
-                    dx /= magnitude
-                    dy /= magnitude
+                        dx /= magnitude
+                        dy /= magnitude
 
-                    player["x"] += dx * SPEED * dt
-                    player["y"] += dy * SPEED * dt
+                        player["x"] += dx * SPEED * dt
+                        player["y"] += dy * SPEED * dt
 
-                    player["x"] = max(
-                        PLAYER_RADIUS,
-                        min(
-                            MAP_SIZE - PLAYER_RADIUS,
-                            round(player["x"])
-                        )
-                    )
-
-                    player["y"] = max(
-                        PLAYER_RADIUS,
-                        min(
-                            MAP_SIZE - PLAYER_RADIUS,
-                            round(player["y"])
-                        )
-                    )
-
-                    if player["has_flag"]:
-                        self.flag["x"] = player["x"]
-                        self.flag["y"] = player["y"]
-
-                    # Verificar victoria
-                    if player["has_flag"]:
-
-                        center = MAP_SIZE / 2
-
-                        distance = math.hypot(
-                            player["x"] - center,
-                            player["y"] - center
-                        )
-
-                        if distance > (CIRCLE_RADIUS + PLAYER_RADIUS):
-
-                            self.is_game_over = True
-                            self.winner_name = player["name"]
-
-                            print(
-                                f"[ENGINE] {self.winner_name} ganó la partida."
+                        player["x"] = max(
+                            PLAYER_RADIUS,
+                            min(
+                                MAP_SIZE - PLAYER_RADIUS,
+                                round(player["x"])
                             )
-                
-                players_list = []
-                for p_id, p_data in self.players.items():
-                    # Creamos una copia para no alterar la memoria original
-                    player_info = p_data.copy()
-                    # Inyectamos el ID como una propiedad interna exigida por los clientes
-                    player_info["id"] = p_id
-                    players_list.append(player_info)
+                        )
 
-                # Construimos el mensaje usando la NUEVA lista en lugar del diccionario
-                state_message = build_message(
-                    "state",
-                    players=players_list,
-                    flag=self.flag
-                )
+                        player["y"] = max(
+                            PLAYER_RADIUS,
+                            min(
+                                MAP_SIZE - PLAYER_RADIUS,
+                                round(player["y"])
+                            )
+                        )
+
+                        if player["has_flag"]:
+                            self.flag["x"] = player["x"]
+                            self.flag["y"] = player["y"]
+
+                        # Verificar victoria
+                        if player["has_flag"]:
+
+                            center = MAP_SIZE / 2
+
+                            distance = math.hypot(
+                                player["x"] - center,
+                                player["y"] - center
+                            )
+
+                            if distance > (CIRCLE_RADIUS + PLAYER_RADIUS):
+
+                                self.is_game_over = True
+
+                                self.game_state = "GAME_OVER"
+                                print("[ENGINE] Fin de la partida.")
+
+                                self.winner_name = player["name"]
+
+                                print(
+                                    f"[ENGINE] {self.winner_name} ganó la partida."
+                                )
+                    
+                    players_list = []
+                    for p_id, p_data in self.players.items():
+                        # Creamos una copia para no alterar la memoria original
+                        player_info = p_data.copy()
+                        # Inyectamos el ID como una propiedad interna exigida por los clientes
+                        player_info["id"] = p_id
+                        players_list.append(player_info)
+
+                    # Construimos el mensaje usando la NUEVA lista en lugar del diccionario
+                    state_message = build_message(
+                        "state",
+                        players=players_list,
+                        flag=self.flag,
+                        game_state=self.game_state,
+                        winner=self.winner_name
+                    )
+
+                if self.game_state == "GAME_OVER":
+                    if self.is_game_over:
+                        self.game_state = "LOBBY"
+                        self.is_game_over = False
+                        self.winner_name = None
+                        center = MAP_SIZE // 2
+                        self.flag["carrier_id"] = None
+                        self.flag["x"] = center
+                        self.flag["y"] = center
+
+                        for player in self.players.values():
+
+                            player["has_flag"] = False
+
+                            player["dir_x"] = 0
+                            player["dir_y"] = 0
+
+                            player["x"] = random.choice([
+                                random.randint(PLAYER_RADIUS,150),
+                                random.randint(850,985)
+                            ])
+
+                            player["y"] = random.choice([
+                                random.randint(PLAYER_RADIUS,150),
+                                random.randint(850,985)
+                            ])
+
+                        print("[ENGINE] Regresando al Lobby.")
             
             # 2. Transmitimos el estado a todos los clientes conectados
             if self.players:  # Solo transmitimos si hay al menos un jugador
